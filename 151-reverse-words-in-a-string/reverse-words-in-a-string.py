8b1dd640274eb0d459ee0e036ef5b3f134e3e100
ljubankrstic <72251950+ljubankrstic@users.noreply.github.com>
2025-08-15 18:14:56 +0200

    Time: 3 ms (38.38%) | Memory: 12.6 MB (32.51%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,8 +1,15 @@
 class Solution(object):
     def reverseWords(self, s):
+        #prvo otklonimo bilo koje prazno mijesto
+        #na pocetku i na kraju stringa
         mystr= s.strip()
+        #zatim napravimo listu rasclanjenjem stringa
+        #napomena: da smo stavili s.split(' '), onda bi
+        #otklonili samo jedan whitespace
         lst = mystr.split()
+        #'prevrtanje' liste
         lst = lst[::-1]
+        #funkcija vraca elemente liste spojene separatorom (razmakom)
         sep = ' '
         return sep.join(lst)
         
